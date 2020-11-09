14810dd6bbc562f3d39e0af10f71671788fc4d32
Joseph Raynovic <Josephraynovic@gmail.com>
2020-11-09 10:02:07 -0800

    Git test for austin

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,3 +1,3 @@
 
-export const baseUrl = 'http://192.168.0.2:5001/';
-// export const baseUrl = 'http://192.168.1.23:5001/';
+// export const baseUrl = 'http://192.168.0.2:5001/';
+export const baseUrl = 'http://192.168.1.23:5001/';
